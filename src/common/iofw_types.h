4e974d2322798f57a8a9a25e080706572a61b38d
nEver <never.wencan@gmail.com>
2012-08-27 11:22:18 +0800

    add iofw_types.h

---
+++ src/common/iofw_types.h
@@ -0,0 +1,45 @@
+/****************************************************************************
+ *       Filename:  iofw_types.h
+ *
+ *    Description:  define data types in iofw
+ *
+ *        Version:  1.0
+ *        Created:  08/20/2012 03:30:19 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Wang Wencan 
+ *	    Email:  never.wencan@gmail.com
+ *        Company:  HPC Tsinghua
+ ***************************************************************************/
+#include "netcdf.h"
+
+#define IOFW_BYTE   NC_BYTE
+#define IOFW_CHAR   NC_CHAR
+#define IOFW_SHORT  NC_SHORT
+#define IOFW_INT    NC_INT
+#define IOFW_FLOAT  NC_FLOAT
+#define IOFW_DOUBLE NC_DOUBLE
+
+#define iofw_types_size(size, type) \
+    do{				    \
+    switch(type) {		    \
+	case IOFW_BYTE :	    \
+	    size = 1;		    \
+	    break;		    \
+	case IOFW_CHAR :	    \
+	    size = 1;		    \
+	    break;		    \
+	case IOFW_SHORT :	    \
+	    size = sizeof(short);   \
+	    break;		    \
+	case IOFW_INT :		    \
+	    size = sizeof(int);	    \
+	    break;		    \
+	case IOFW_FLOAT :	    \
+	    size = sizeof(float);   \
+	    break;		    \
+	case IOFW_DOUBLE :	    \
+	    size = sizeof(double);  \
+	    break;		    \
+    }} while(0)
